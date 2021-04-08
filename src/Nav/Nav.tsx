6cbd916a77d7f0042f435104efe1a2d43ad629c4
Roman Radaykin <roman.radaikin@yandex.ru>
2021-04-08 16:34:42 +0300

    'aaa'

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -9,7 +9,7 @@ const Nav = () => {
         <NavLink to={'/profile'}>Profile</NavLink>
         <NavLink to={'/404'}>Error404</NavLink>
         <NavLink to={'/password-recovery'}>Password recovery</NavLink>
-        <NavLink to={'/enter-new-password'}>New password</NavLink>
+        <NavLink to={'/enter-new-password/:token'}>New password</NavLink>
         <NavLink to={'/show-all'}>Show all</NavLink>
     </nav>
 }
